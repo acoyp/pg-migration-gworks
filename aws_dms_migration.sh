7902026a213a5c1d8a205093247e4e8c6a08dfd3
acoyp99 <arley.c@thinkbridge.com>
2023-05-30 16:11:10 -0500

    no aws cli command are executing

--- aws_dms_migration.sh
+++ aws_dms_migration.sh
@@ -21,17 +21,17 @@ TARGET_ENGINE_NAME="postgres"
 
 # Create source endpoint
 if [ `aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$SOURCE_ENDPOINT_IDENTIFIER'].EndpointIdentifier" --output text` ]; then
-    echo "AWS DMS Endpoint already exists..."
+  echo "AWS DMS Endpoint already exists..."
 else
-    aws dms create-endpoint \
-    --endpoint-identifier "$SOURCE_ENDPOINT_IDENTIFIER" \
-    --endpoint-type "source" \
-    --engine-name "$SOURCE_ENGINE_NAME" \
-    --server-name "$SOURCE_SERVER_NAME" \
-    --port "5432" \
-    --database-name "$SOURCE_DATABASE_NAME" \
-    --username "$SOURCE_USERNAME" \
-    --password "$SOURCE_PASSWORD"
+  aws dms create-endpoint \
+  --endpoint-identifier "$SOURCE_ENDPOINT_IDENTIFIER" \
+  --endpoint-type "source" \
+  --engine-name "$SOURCE_ENGINE_NAME" \
+  --server-name "$SOURCE_SERVER_NAME" \
+  --port "5432" \
+  --database-name "$SOURCE_DATABASE_NAME" \
+  --username "$SOURCE_USERNAME" \
+  --password "$SOURCE_PASSWORD"
 fi
 
 # Create target endpoint
@@ -50,8 +50,8 @@ else
 fi
 
 TASK_IDENTIFIER="my-replication-task-gWorks"
-SOURCE_ENDPOINT_ARN=`aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$SOURCE_ENDPOINT_IDENTIFIER'].EndpointArn" --output text`
-TARGET_ENDPOINT_ARN=`aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$TARGET_ENDPOINT_IDENTIFIER'].EndpointArn" --output text`
+SOURCE_ENDPOINT_ARN=$(aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$SOURCE_ENDPOINT_IDENTIFIER'].EndpointArn" --output text)
+TARGET_ENDPOINT_ARN=$(aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$TARGET_ENDPOINT_IDENTIFIER'].EndpointArn" --output text)
 MIGRATION_TYPE="full-load"
 TABLE_MAPPINGS_FILE="table_mapping.json"
 
