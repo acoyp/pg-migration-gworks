5a125646d92bedc1ae95d495ad990b89d235634f
acoyp99 <arley.c@thinkbridge.com>
2023-05-30 19:40:45 -0500

    test variables doesn't work

--- aws_dms_migration.sh
+++ aws_dms_migration.sh
@@ -23,35 +23,37 @@ TARGET_ENGINE_NAME="postgres"
 if [ `aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$SOURCE_ENDPOINT_IDENTIFIER'].EndpointIdentifier" --output text` ]; then
   echo "AWS DMS Endpoint already exists..."
 else
-  aws dms create-endpoint \
-  --endpoint-identifier "$SOURCE_ENDPOINT_IDENTIFIER" \
-  --endpoint-type "source" \
-  --engine-name "$SOURCE_ENGINE_NAME" \
-  --server-name "$SOURCE_SERVER_NAME" \
-  --port "5432" \
-  --database-name "$SOURCE_DATABASE_NAME" \
-  --username "$SOURCE_USERNAME" \
-  --password "$SOURCE_PASSWORD"
+  echo "AWS DMS doesn't exists..."
+  # aws dms create-endpoint \
+  #   --endpoint-identifier "$SOURCE_ENDPOINT_IDENTIFIER" \
+  #   --endpoint-type "source" \
+  #   --engine-name "$SOURCE_ENGINE_NAME" \
+  #   --server-name "$SOURCE_SERVER_NAME" \
+  #   --port "5432" \
+  #   --database-name "$SOURCE_DATABASE_NAME" \
+  #   --username "$SOURCE_USERNAME" \
+  #   --password "$SOURCE_PASSWORD"
 fi
 
 # Create target endpoint
 if [ `aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$TARGET_ENDPOINT_IDENTIFIER'].EndpointIdentifier" --output text` ]; then
-    echo "AWS DMS Endpoint already exists..."
+  echo "AWS DMS Endpoint already exists..."
 else
-  aws dms create-endpoint \
-    --endpoint-identifier "$TARGET_ENDPOINT_IDENTIFIER" \
-    --endpoint-type "target" \
-    --engine-name "$TARGET_ENGINE_NAME" \
-    --server-name "$TARGET_SERVER_NAME" \
-    --database-name "$TARGET_DATABASE_NAME" \
-    --port "5432" \
-    --username "$TARGET_USERNAME" \
-    --password "$TARGET_PASSWORD"
+  echo "AWS DMS doesn't exists..."
+  # aws dms create-endpoint \
+  #   --endpoint-identifier "$TARGET_ENDPOINT_IDENTIFIER" \
+  #   --endpoint-type "target" \
+  #   --engine-name "$TARGET_ENGINE_NAME" \
+  #   --server-name "$TARGET_SERVER_NAME" \
+  #   --database-name "$TARGET_DATABASE_NAME" \
+  #   --port "5432" \
+  #   --username "$TARGET_USERNAME" \
+  #   --password "$TARGET_PASSWORD"
 fi
 
 TASK_IDENTIFIER="my-replication-task-gWorks"
-SOURCE_ENDPOINT_ARN=$(aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$SOURCE_ENDPOINT_IDENTIFIER'].EndpointArn" --output text)
-TARGET_ENDPOINT_ARN=$(aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$TARGET_ENDPOINT_IDENTIFIER'].EndpointArn" --output text)
+SOURCE_ENDPOINT_ARN=`aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$SOURCE_ENDPOINT_IDENTIFIER'].EndpointArn" --output text`
+TARGET_ENDPOINT_ARN=`aws dms describe-endpoints --query "Endpoints[?EndpointIdentifier=='$TARGET_ENDPOINT_IDENTIFIER'].EndpointArn" --output text`
 MIGRATION_TYPE="full-load"
 TABLE_MAPPINGS_FILE="table_mapping.json"
 echo $SOURCE_ENDPOINT_ARN
