532e4ccc6ffc43b5d41042e232515e57c8f643a3
acoyp99 <arley.c@thinkbridge.com>
2023-06-01 01:28:05 -0500

    Caps

--- json_modify.sh
+++ json_modify.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cat compareDBs.json
-compareDB_data=`cat compareDBs.json`
-compareDB_data=`jq ".connection1.host = \"$SOURCE_SERVER_NAME\"" <<< "$compareDB_data"`
-compareDB_data=`jq ".connection1.database = \"$SOURCE_DATABASE_NAME\"" <<< "$compareDB_data"`
-compareDB_data=`jq ".connection1.user = \"$SOURCE_USERNAME\"" <<< "$compareDB_data"` 
-compareDB_data=`jq ".connection1.password = \"$SOURCE_PASSWORD\"" <<< "$compareDB_data"` 
-compareDB_data=`jq ".connection2.host = \"$TARGET_SERVER_NAME\"" <<< "$compareDB_data"` 
-compareDB_data=`jq ".connection2.database = \"$TARGET_DATABASE_NAME\"" <<< "$compareDB_data"` 
-compareDB_data=`jq ".connection2.user = \"$TARGET_USERNAME\"" <<< "$compareDB_data"` 
-compareDB_data=`jq ".connection2.password = \"$TARGET_PASSWORD\"" <<< "$compareDB_data" > compareDBs.json` 
+DB_DATA=`cat compareDBs.json`
+DB_DATA=`jq ".connection1.host = \"$SOURCE_SERVER_NAME\"" <<< "$DB_DATA"`
+DB_DATA=`jq ".connection1.database = \"$SOURCE_DATABASE_NAME\"" <<< "$DB_DATA"`
+DB_DATA=`jq ".connection1.user = \"$SOURCE_USERNAME\"" <<< "$DB_DATA"` 
+DB_DATA=`jq ".connection1.password = \"$SOURCE_PASSWORD\"" <<< "$DB_DATA"` 
+DB_DATA=`jq ".connection2.host = \"$TARGET_SERVER_NAME\"" <<< "$DB_DATA"` 
+DB_DATA=`jq ".connection2.database = \"$TARGET_DATABASE_NAME\"" <<< "$DB_DATA"` 
+DB_DATA=`jq ".connection2.user = \"$TARGET_USERNAME\"" <<< "$DB_DATA"` 
+DB_DATA=`jq ".connection2.password = \"$TARGET_PASSWORD\"" <<< "$DB_DATA" > compareDBs.json` 
 
 cat compareDBs.json
