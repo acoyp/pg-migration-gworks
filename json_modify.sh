db052c80d46fd26b0dea5fc6dae86a17f001733e
acoyp99 <arley.c@thinkbridge.com>
2023-06-01 00:56:56 -0500

    test issue redirection

--- json_modify.sh
+++ json_modify.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-compareDB_data=$(cat compareDBs.json)
+compareDB_data=$(cat ./compareDBs.json)
 compareDB_data=$(jq '.connection1.host = "${SOURCE_SERVER_NAME}"' <<< "$compareDB_data")
 compareDB_data=$(jq '.connection1.database = "${SOURCE_DATABASE_NAME}"' <<< "$compareDB_data")
 compareDB_data=$(jq '.connection1.user = "${SOURCE_USERNAME}"' <<< "$compareDB_data") 
@@ -8,6 +8,6 @@ compareDB_data=$(jq '.connection1.password = "${SOURCE_PASSWORD}"' <<< "$compare
 compareDB_data=$(jq '.connection2.host = "${TARGET_SERVER_NAME}"' <<< "$compareDB_data") 
 compareDB_data=$(jq '.connection2.database = "$(TARGET_DATABASE_NAME)"' <<< "$compareDB_data") 
 compareDB_data=$(jq '.connection2.user = "${TARGET_USERNAME}"' <<< "$compareDB_data") 
-compareDB_data=$(jq '.connection2.password = "${TARGET_PASSWORD}"' <<< "$compareDB_data" > compareDBs.json) 
+compareDB_data=$(jq '.connection2.password = "${TARGET_PASSWORD}"' <<< "$compareDB_data" > ./compareDBs.json) 
 
-cat compareDBs.json
+cat ./compareDBs.json
